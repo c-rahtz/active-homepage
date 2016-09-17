7e87e942b46e3cd9efe4d512aab90530b761b197
c-rahtz <c.rahtz@gmail.com>
2016-09-16 20:53:01 -0400

    adding Sass task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,38 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	concat = require('gulp-concat');
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify');
 
 var jsSources = [
-	'components/scripts/docready.min.js',
-	'components/scripts/action.js'
-]
+    'components/scripts/docready.min.js',
+    'components/scripts/action.js'
+];
+
+var sassSources = [
+    'components/sass/rahtz.net.scss'
+];
+
 
 gulp.task('js', function() {
-	gulp.src(jsSources)
-		.pipe(concat('action.js'))
-		.pipe(gulp.dest('builds/development/js'));
+    gulp.src(jsSources)
+        .pipe(concat('action.js'))
+        .pipe(gulp.dest('builds/development/js'));
 });
 
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/img',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+})
+
 gulp.task('default', function() {
-	gutil.log("here we are.");
+    gutil.log("here we are.");
 });
